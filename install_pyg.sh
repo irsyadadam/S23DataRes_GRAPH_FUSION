b178ed261c243fde28705c4f6de63619ddaba95b
Irsyad Adam <59972182+irsyadadam@users.noreply.github.com>
2023-05-04 17:48:05 -0700

    conda env

---
+++ install_pyg.sh
@@ -0,0 +1,11 @@
+conda install -y pip 
+
+conda install -y tqdm
+conda install -y pandas
+conda install -y numpy
+conda install -y ipykernel
+conda install -y jupyter
+conda install -y seaborn
+conda install -y networkx
+conda install -y pytorch==1.11.0 -c pytorch
+conda install -y pyg -c pyg 
