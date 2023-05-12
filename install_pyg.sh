6f441602696d871109783480d54d3b1c1fa76991
Irsyad Adam <59972182+irsyadadam@users.noreply.github.com>
2023-05-11 17:36:01 -0700

    Update install_pyg.sh
    change torch v

--- install_pyg.sh
+++ install_pyg.sh
@@ -7,5 +7,5 @@ conda install -y ipykernel
 conda install -y jupyter
 conda install -y seaborn
 conda install -y networkx
-conda install -y pytorch==1.11.0 -c pytorch
+conda install -y pytorch::pytorch torchvision torchaudio -c pytorch
 conda install -y pyg -c pyg 
